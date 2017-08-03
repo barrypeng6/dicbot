2350af664e710d20e7fea3d4a489c85a718d0dff
Barry <barrypeng6@gmail.com>
2017-08-03 16:49:43 +0800

    init Linebot & Oxford DIC API

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
-const fs = require('fs');
+const request = require('request');
 
 const config = {
-  channelAccessToken: '/oDyzG4L2rx00RWJJ2UUb6xjEEwUBYy1AhkKnEiPQImTnViTOpuu4j7yKLUHtkNw7YPookYMjSYd1xImN5pV5MWeXDonJZ737ztf4+xzQPjvrrHCg2RIN9RewhsFrNdh30ZTLc/yTgtHosjOBQEguwdB04t89/1O/w1cDnyilFU=',
-  channelSecret: 'ae1af91782cb133bc14058b445f64ebc'
+  channelAccessToken: '',
+  channelSecret: ''
 };
+
+const app_id = ''
+const app_key = ''
 
 const client = new line.Client(config);
 const app = express();
 
 app.post('/webhook', line.middleware(config), (req, res) => {
-  Promise.all(req.body.events.map(handleEvent)).then(result =>
-    res.json(result)
-  );
+  Promise.all(req.body.events.map(handleEvent)).then(result => {
+    return res.json(result)
+  });
+  // return res.sendStatus(200)
 });
 
 function handleEvent(event) {
@@ -21,10 +25,31 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
 
-  const echo = { type: 'text', text: 'Hi！' };
+  const options = {
+    url: `https://od-api.oxforddictionaries.com/api/v1/entries/en/${event.message.text}`,
+    headers: {
+      'app_id': app_id,
+      'app_key': app_key
+    }
+  };
 
-  return client.replyMessage(event.replyToken, echo);
+  request(options, (error, res, body) => {
+    if (!error && res.statusCode == 200) {
+      let result = JSON.parse(body);
+      let senses = result.results[0].lexicalEntries[0].entries[0].senses;
+      console.log('>>', senses);
+      let answer = senses.reduce((acc, e, i) => {
+        return acc + (i+1) + ': ' + e.definitions[0] + '\n\n'
+      }, '');
+      let definitions = result.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]
+      return client.replyMessage(event.replyToken, { type: 'text', text: '你查的是' + result.results[0].word + '\n定義：\n' + answer });
+    }
+  });
 }
+
+
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
